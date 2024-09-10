15a167b55de58a44f53f1375d874534e98cabb6f
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-09-10 16:39:19 +0300

    Establish web server listen for incoming HTTP requests

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func dashboard(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Featify"))
+}
+
+func projectView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific project..."))
+}
+
+func projectCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new project..."))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", dashboard)
+	mux.HandleFunc("/project/view", projectView)
+	mux.HandleFunc("/project/create", projectCreate)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
