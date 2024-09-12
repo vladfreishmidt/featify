0258dcb989d261cec554c6de8e9430c0f6c72fa8
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-09-12 15:43:58 +0300

    Implement leveled custom loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -19,7 +23,7 @@ func main() {
 	mux.HandleFunc("/project/view", projectView)
 	mux.HandleFunc("/project/create", projectCreate)
 
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
