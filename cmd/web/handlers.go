4121ffed9559a2aede22e637c8b4f4e4a1fe0b34
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-09-16 11:54:18 +0300

    Implement dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 // dashboard handler.
-func dashboard(w http.ResponseWriter, r *http.Request) {
+func (app *application) dashboard(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -24,20 +23,20 @@ func dashboard(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
 // projectView handler.
-func projectView(w http.ResponseWriter, r *http.Request) {
+func (app *application) projectView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -48,7 +47,7 @@ func projectView(w http.ResponseWriter, r *http.Request) {
 }
 
 // projectCreate handler.
-func projectCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) projectCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
