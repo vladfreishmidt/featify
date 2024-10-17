1485271592636681aecb5e0fa54a63cef0f5d697
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-10-17 19:47:03 +0300

    Set security headers for every response

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/project/create", app.projectCreate)
 	mux.HandleFunc("/projects", app.projectList)
 
-	return mux
+	return secureHeaders(mux)
 }
