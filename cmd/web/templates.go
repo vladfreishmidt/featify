35c933bf4acd9e239b0f4e4d0f2cfa23a335aedd
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-11-20 19:01:38 +0200

    Implement CSRF token logic

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -19,6 +19,7 @@ type templateData struct {
 	Form            any
 	Flash           string
 	IsAuthenticated bool
+	CSRFToken       string
 }
 
 func newTemplateCahce() (map[string]*template.Template, error) {
